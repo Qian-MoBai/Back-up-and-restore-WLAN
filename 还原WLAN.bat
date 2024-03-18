3a1e8981a518bfdc9b68624d765e5e6cb3bc185f
笑肖姣 <121647364+Qian-MoBai@users.noreply.github.com>
2024-03-18 08:22:54 +0800

    更新 还原WLAN.bat

--- 还原WLAN.bat
+++ 还原WLAN.bat
@@ -5,10 +5,10 @@ for %%i in (./*.xml) do (
 	netsh wlan add profile "%%i"
 )
 set /p input=已还原WLAN，是否删除文件？（Y/n）
-if /i %input%=="Y" goto Yes
+if /i %input%=="Y" goto yes
 if /i %input%=="N" goto exit
 
-:Yes
+:yes
 cd ..
 rmdir /s /q WLAN备份
 echo 文件已删除
