7d509e5b53188145748c53290a85c32c101cc3db
Qian-MoBai <Qian-MoBai@outlook.com>
2025-05-04 15:30:33 +0800

    fix(BackupWLAN): 修复 WLAN 备份脚本中的文件路径问题
    - 将相对路径替换为绝对路径，确保脚本在任何目录下都能正确运行
    - 使用 %~dp0 变量获取当前脚本所在目录的路径

--- BackupWLAN.bat
+++ BackupWLAN.bat
@@ -3,7 +3,7 @@ title BackupWLAN
 cd /d "%~dp0"
 md BackupWLAN
 netsh wlan show profile
-netsh wlan export profile folder=.\BackupWLAN key=clear
+netsh wlan export profile folder="%~dp0\BackupWLAN" key=clear
 echo the WLAN has been backed up
 echo generate a zip file
 7za a -tzip BackupWLAN.zip BackupWLAN RestoreWLAN.bat
