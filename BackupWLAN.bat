20d1e86ac5325c6ae1c3d48f91fa93a42f033df4
Qian-MoBai <Qian-MoBai@outlook.com>
2025-05-04 12:56:56 +0800

    bat(BackupWLAN): 优化 WLAN 备份脚本
    - 简化了 echo 语句
    - 添加了删除 BackupWLAN 文件夹的步骤
    - 优化了 Zip 文件创建过程

--- BackupWLAN.bat
+++ BackupWLAN.bat
@@ -4,8 +4,10 @@ cd /d "%~dp0"
 md BackupWLAN
 netsh wlan show profile
 netsh wlan export profile folder=.\BackupWLAN key=clear
-echo "the WLAN has been backed up"
-echo "generate a zip file¡­¡­"
+echo the WLAN has been backed up
+echo generate a zip file
 7za a -tzip BackupWLAN.zip BackupWLAN RestoreWLAN.bat
-echo "press any key to exit"
+@REM delete the folder
+rd  /s /q BackupWLAN
+echo press any key to exit
 pause > nul
--- RestoreWLAN.bat
+++ RestoreWLAN.bat
@@ -4,5 +4,5 @@ cd BackupWLAN
 for %%i in (./*.xml) do (
 	netsh wlan add profile "%%i"
 )
-echo "the WLAN has been restored press any key to exit"
+echo the WLAN has been restored press any key to exit
 pause > nul
